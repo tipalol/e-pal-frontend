9b8c7763ed9b0ac754144e0eea6dabedd946c1e6
Sorokin Dmitriy <tipalol2@gmail.com>
2024-10-26 01:07:43 +0300

    improvments font

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -7,7 +7,7 @@
     </div>
     <!-- Right-side buttons -->
     <div class="flex items-center space-x-4">
-      <button class="text-white font-bold hover:text-purple-300 transition">Contact Us</button>
+      <button class="bg-purple-500 text-white py-1 px-2 rounded-full  hover:bg-purple-600 transition text-sm">Contact Us</button>
       <button class="text-white font-bold hover:text-purple-300 transition">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
