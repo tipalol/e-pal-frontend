7ef82f0b98123662f9eaa23cc002544046b6b2f6
Sorokin Dmitriy <tipalol2@gmail.com>
2024-10-26 01:11:17 +0300

    improvments header

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full p-4 flex justify-between items-center fixed z-50">
+  <header class="w-full p-7 flex justify-between items-center fixed z-50">
     <!-- Logo -->
     <div class="flex items-center">
       <img src="../assets/icons8-google.svg" alt="Logo" class="w-10 h-10">
@@ -7,8 +7,8 @@
     </div>
     <!-- Right-side buttons -->
     <div class="flex items-center space-x-4">
-      <button class="bg-purple-500 text-white py-1 px-2 rounded-full  hover:bg-purple-600 transition text-sm">Contact Us</button>
-      <button class="text-white font-bold hover:text-purple-300 transition">
+      <button class="bg-purple-500 text-white py-1 px-3 rounded-full  hover:bg-purple-600 transition text-sm">Contact Us</button>
+      <button class="bg-gray-700 rounded-full text-white font-bold hover:text-purple-300 transition p-1">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
         </svg>
