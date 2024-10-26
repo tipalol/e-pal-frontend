d8aa7336ad8ebbf2d6a2f0ebd2ec66d6d536cd6f
Sorokin Dmitriy <tipalol2@gmail.com>
2024-10-27 01:43:01 +0300

    improve email validation page

--- src/pages/registration/views/VerificationPage.vue
+++ src/pages/registration/views/VerificationPage.vue
@@ -8,6 +8,7 @@
         placeholder="Enter Verification Code"
         class="w-full px-4 py-3 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring focus:border-purple-400"
     />
+    <p @click="resendCode()" class="text-gray-400 text-center flex justify-start mt-6 cursor-pointer hover:text-purple-500">Resend email confirmation letter</p>
     <div class="flex justify-end mt-6">
       <button
           :disabled="verificationCode.length < 5"
@@ -53,6 +54,20 @@ export default {
         console.error("Error during verification:", error);
       }
     },
+    async resendCode() {
+      try {
+        const response = await axios.post("http://localhost:5033/api/register/resend?email="+this.email);
+        if (response.data) {
+          console.log("Resend successful:", response.data);
+        } else {
+          this.error = "Can't resend email. Please try again later.";
+          console.error("Error during resending:", response.data);
+        }
+      } catch (error) {
+        this.error = "Can't resend email. Please try again later.";
+        console.error("Error during resending:", error);
+      }
+    }
   },
 };
 </script>
