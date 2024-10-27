93712b636ae742456d769a392e96d7a03615521a
Sorokin Dmitriy <tipalol2@gmail.com>
2024-10-27 17:35:13 +0300

    added main page components

--- src/pages/main/Main.vue
+++ src/pages/main/Main.vue
@@ -2,19 +2,7 @@
   <div class="bg-gray-900 text-white min-h-screen">
     <Header />
 
-    <!-- Banner -->
-    <section
-        class="relative bg-purple-700 p-10 bg-center bg-no-repeat bg-cover h-[400px] flex items-center"
-        :style="{ backgroundImage: `url('banner.webp')` }"
-    >
-      <div class="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-40"></div>
-
-      <!-- Left-aligned, vertically centered text -->
-      <h1 class="text-4xl font-bold text-white relative z-10 font-display ml-10">
-        FIRST RECHARGE BONUS!
-      </h1>
-    </section>
-
+    <Banner />
 
     <!-- Catalog Section -->
     <section class="p-8">
@@ -22,9 +10,8 @@
 
       <!-- Service Cards -->
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-        <div v-for="service in services" :key="service.name" class="bg-gray-800 rounded-lg p-4 text-center">
+        <div v-for="service in services" :key="service.name" class="bg-gray-800 rounded-lg text-center">
           <img :src="service.image" :alt="service.name" class="h-40 w-full object-cover rounded-lg mb-4" />
-          <h3 class="font-bold text-lg">{{ service.name }}</h3>
         </div>
       </div>
     </section>
@@ -33,9 +20,10 @@
 
 <script>
 import Header from "../common/header/Header.vue";
+import Banner from "./components/Banner.vue";
 
 export default {
-  components: {Header},
+  components: {Banner, Header},
   data() {
     return {
       services: [
---
+++ src/pages/main/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Banner -->
+  <section
+      class="relative bg-purple-700 p-10 bg-center bg-no-repeat bg-cover h-[400px] flex items-center"
+      :style="{ backgroundImage: `url('banner.webp')` }"
+  >
+    <div class="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-40"></div>
+
+    <!-- Left-aligned, vertically centered text -->
+    <h1 class="text-4xl font-bold text-white relative z-10 font-display ml-10">
+      FIRST RECHARGE BONUS!
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Banner"
+}
+</script>
