5046d412ba292e519a045dc99da011df19586a36
Sorokin Dmitriy <tipalol2@gmail.com>
2024-10-30 20:38:15 +0300

    make epal card clickable

--- src/pages/common/EpalCard.vue
+++ src/pages/common/EpalCard.vue
@@ -1,14 +1,13 @@
 <template>
+  <router-link :to=getProfileUrl()>
   <img
       :src="this.avatar"
       alt="user profile"
       class="w-16 h-16 rounded-full absolute -top-8 left-4 border-4 border-gray-900"
   />
 
-  <!-- Online Status Dot -->
   <div class="absolute top-2 left-12 w-3 h-3 bg-green-500 rounded-full border-2 border-gray-900"></div>
 
-  <!-- User Details -->
   <div class="mt-8">
     <h3 class="font-semibold text-lg">{{ this.username }}</h3>
     <div class="flex items-center text-sm text-yellow-400 mt-1">
@@ -17,11 +16,11 @@
     <p class="text-gray-400 text-xs mt-2">{{ this.bio }}</p>
   </div>
 
-  <!-- Service Info -->
   <div class="flex items-center justify-between mt-4 text-sm">
     <span class="bg-gray-700 px-2 py-1 rounded-full">Chill</span>
     <span class="text-purple-400 font-semibold">{{ 6.66 }}/time</span>
   </div>
+  </router-link>
 </template>
 
 <script>
@@ -45,5 +44,10 @@ export default {
       required: true,
     },
   },
+  methods: {
+    getProfileUrl() {
+      return "/profile/" + this.username;
+    }
+  }
 }
 </script>
